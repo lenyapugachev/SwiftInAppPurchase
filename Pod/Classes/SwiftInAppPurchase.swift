063f7b0a710892d10030aa265db2957568670ae6
lenyapugachev <lenyapugachev@me.com>
2018-10-10 16:36:26 +0700

    Update SwiftInAppPurchase.swift
    Swift 3.x

--- Pod/Classes/SwiftInAppPurchase.swift
+++ Pod/Classes/SwiftInAppPurchase.swift
@@ -40,30 +40,30 @@ public class SwiftInAppPurchase: NSObject {
     public func productForIdentifier(productIdentifier:String) -> SKProduct{
         return self.productRequestHandler.products[productIdentifier]!
     }
-    public func requestProducts(productIDS:Set<String>,completion:RequestProductCallback){
-        self.productRequestHandler.requestProduc(productIDS, requestCallback: completion)
+    public func requestProducts(productIDS:Set<String>,completion:@escaping RequestProductCallback){
+        self.productRequestHandler.requestProduc(productIds: productIDS, requestCallback: completion)
     }
     //  MARK: - Purchase
-    public func addPayment(productIDS: String,userIdentifier:String?, addPaymentCallback: AddPaymentCallback){
+    public func addPayment(productIDS: String,userIdentifier:String?, addPaymentCallback: @escaping AddPaymentCallback){
         let product = self.productRequestHandler.products[productIDS]
         if product != nil {
-            self.paymentRequestHandler.addPayment(product!, userIdentifier: userIdentifier, addPaymentCallback: addPaymentCallback)
+            self.paymentRequestHandler.addPayment(product: product!, userIdentifier: userIdentifier, addPaymentCallback: addPaymentCallback)
         }else{
-            addPaymentCallback(result:.Failed(error: NSError.init(domain: "AddPayment Unknow Product identifier", code: 0, userInfo: nil)))
+            addPaymentCallback(.Failed(error: NSError.init(domain: "AddPayment Unknow Product identifier", code: 0, userInfo: nil)))
         }
     }
     //  MARK: - Restore
-    public func restoreTransaction(userIdentifier:String?,addPaymentCallback: AddPaymentCallback){
-        self.paymentRequestHandler.restoreTransaction(userIdentifier, addPaymentCallback: addPaymentCallback)
+    public func restoreTransaction(userIdentifier:String?,addPaymentCallback: @escaping AddPaymentCallback){
+        self.paymentRequestHandler.restoreTransaction(userIdentifier: userIdentifier, addPaymentCallback: addPaymentCallback)
     }
-    public func checkIncompleteTransaction(addPaymentCallback: AddPaymentCallback){
-        self.paymentRequestHandler.checkIncompleteTransaction(addPaymentCallback)
+    public func checkIncompleteTransaction(addPaymentCallback: @escaping AddPaymentCallback){
+        self.paymentRequestHandler.checkIncompleteTransaction(addPaymentCallback: addPaymentCallback)
     }
     //  MARK: - Receipt
-    public func refreshReceipt(requestCallback: RequestReceiptCallback){
-        self.receiptRequestHandler.refreshReceipt(requestCallback)
+    public func refreshReceipt(requestCallback: @escaping RequestReceiptCallback){
+        self.receiptRequestHandler.refreshReceipt(requestCallback: requestCallback)
     }
-    public func verifyReceipt(autoRenewableSubscriptionsPassword:String?,receiptVerifyCallback:ReceiptVerifyCallback){
-        self.receiptRequestHandler.verifyReceipt(autoRenewableSubscriptionsPassword, receiptVerifyCallback: receiptVerifyCallback)
+    public func verifyReceipt(autoRenewableSubscriptionsPassword:String?,receiptVerifyCallback:@escaping ReceiptVerifyCallback){
+        self.receiptRequestHandler.verifyReceipt(autoRenewableSubscriptionsPassword: autoRenewableSubscriptionsPassword, receiptVerifyCallback: receiptVerifyCallback)
     }
 }
